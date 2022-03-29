f60f2b43030bcaa056484eb88d2fd35ad5a0f88c
Joseph <softjoe40@gmail.com>
2022-03-29 11:02:38 +0100

    created my own authorization middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,51 +1,18 @@
 const express = require("express")
-const { products } = require("./data")
-const https = require("https")
 const app = express()
+const logger = require("./logger")
+const authorize = require("./authorize")
 
-app.get("/", function (req, res) {
-    res.send(`<h1>Homepage</h1><a href="/api/products">products</a>`)
+app.use(authorize)
+
+app.get("/", (req, res) => {
+    res.send("hello world")
 })
 
-app.get("/api/products", (req, res) => {
-    const allProduct = products.map((product) => {
-        const { id, name, image } = product
-        return { id, name, image }
-    })
-
-    res.json(allProduct)
+app.get("/authorize", (req, res) => {
+    res.send("Authorize")
 })
 
-app.get("/api/products/:productId", (req, res) => {
-    const { productId } = req.params
-    const singleProduct = products.find((product) => product.id === Number(productId))
-    if (!singleProduct) {
-        res.status(404)
-        res.json({ "error": "not found" })
-    }
-    res.json(singleProduct)
-})
-
-app.get("/api/v1/query", (req, res) => {
-    const { search, limit } = req.query
-    let sortedProduct = [...products]
-
-    if (search) {
-        sortedProduct = sortedProduct.filter((products) => {
-            return products.name.startsWith(search)
-        })
-    }
-
-    if (limit) {
-        sortedProduct = sortedProduct.slice(0, Number(limit))
-    }
-
-    if (sortedProduct < 1) {
-        return res.status.json({success: true, dat})
-    }
-    res.status(200).json(sortedProduct)
-})
-
-app.listen(3000, function () {
-    console.log(`http://localhost:3000`)
+app.listen(5000, () => {
+    console.log(`http://localhost:5000`)
 })
---
+++ 02-express-tutorial/logger.js
@@ -0,0 +1,9 @@
+const logger = function (req, res, next) {
+    const method = req.method
+    const url = req.url
+    const status = req.status
+    console.log(method, url, status)
+    next()
+}
+
+module.exports = logger
