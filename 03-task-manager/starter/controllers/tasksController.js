6e7f5cb6f70739fa129f729399fd804739532884
Joseph-01 <softjoe40@gmail.com>
2022-05-10 20:31:16 +0100

    create dummy controller for routes

---
+++ 03-task-manager/starter/controllers/tasksController.js
@@ -0,0 +1,33 @@
+// const res = require("express/lib/response");
+
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const getTaskById = (req, res)=>{
+    res.send("id items");
+}
+
+const createNewTask = (req, res) => {
+    
+    // res.json({"hello":"create task 201"})
+    res.send(req.body)
+}
+
+
+
+const updateTask = (req, res) => {
+    res.json({"hello":"update task 200"})
+}
+
+const deleteTask = (req, res) => {
+    res.send("dalete an item");
+}
+
+module.exports = {
+  getAllTasks,
+  createNewTask,
+  getTaskById,
+  updateTask,
+  deleteTask
+};
