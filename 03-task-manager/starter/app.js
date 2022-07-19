597c11d833fa9b70aadbe86ef546e02ae0fcd0f3
Joseph-01 <softjoe40@gmail.com>
2022-07-19 07:47:19 +0100

    create middleware to handle asnyc, try and catch request

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/task");
 const { urlencoded } = require("express");
+const notFound = require("./middleware/notFound")
+
+
 
 app.use(express.static("public"))
 //middlewarethat returns json data
@@ -10,7 +13,7 @@ app.use(express.json());
 
 //use api router
 app.use("/api/v1/tasks", tasks);
-
+app.use(notFound)
 
 //use the start dbconnection to start connection
 const port = 3000
