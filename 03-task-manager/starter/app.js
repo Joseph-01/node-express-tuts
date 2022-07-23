b6c0566241b28a1fe913fbc5bff49712ca3334ee
Joseph-01 <softjoe40@gmail.com>
2022-07-23 11:05:23 +0100

    finished task manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,6 +4,8 @@ const app = express();
 const tasks = require("./routes/task");
 const { urlencoded } = require("express");
 const notFound = require("./middleware/notFound")
+const errorHandler = require("./middleware/errorHandler")
+require("dotenv").config()
 
 
 
@@ -14,9 +16,10 @@ app.use(express.json());
 //use api router
 app.use("/api/v1/tasks", tasks);
 app.use(notFound)
+app.use(errorHandler)
 
 //use the start dbconnection to start connection
-const port = 3000
+const port = process.env.PORT || 3000
 connectDb.startConnection(app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 }))
--- 03-task-manager/starter/controllers/tasksController.js
+++ 03-task-manager/starter/controllers/tasksController.js
@@ -1,23 +1,25 @@
 //use our task model
 const Task = require("../models/TaskModel")
 const asyncWrapper = require("../middleware/async")
+const { createCustomError } = require("../errors/custom-error")
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
 });
 
-const getTaskById = asyncWrapper(async (req, res) => {
+const getTaskById = asyncWrapper(async (req, res, next) => {
   const taskId = req.params.id;
   const task = await Task.findById(taskId)
-  if (task) {
-    return res.status(200).json({ task })
+  if (!task) {
+    return next(createCustomError(`task id ${taskId} not found`, 404))
   }
+  res.status(200).json({ task })
 })
 
 const createNewTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body)
-  return res.status(201).json({ task })
+  res.status(201).json({ task })
 })
 
 
@@ -32,7 +34,7 @@ const updateTask = asyncWrapper(async (req, res) => {
       runValidators: true
     })
   if (!task) {
-    return res.status(404).json({ "msg": `task id ${taskId} not found` })
+    return next(createCustomError(`task id ${taskId} not found`, 404))
   }
   res.status(200).json({ task })
 })
@@ -41,7 +43,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskId } = req.params
   const task = await Task.findOneAndDelete({ _id: taskId })
   if (!task) {
-    return res.status(404).json({ "msg": `task id ${taskId} not found` })
+    return next(createCustomError(`task id ${taskId} not found`, 404))
   }
   res.status(200).json({ task })
 })
